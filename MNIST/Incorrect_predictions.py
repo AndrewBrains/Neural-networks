ed4a8d3f5473b8b0c66f90539afd19c85b12d63d
AndrewBrains <paa1401@mail.ru>
2019-10-09 20:53:14 +0300

    Create Incorrect_predictions.py

---
+++ MNIST/Incorrect_predictions.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(50, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
+predictions = model.predict(x_train[0:60000,:,:])
+err=[]
+for i in range (0,60000):
+    if (predictions[i].argmax()!=y_train[i]):
+        err.append(i)
+print(err)
